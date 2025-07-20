5ae2f6e8e0f480682c43af5d85328b1e0e5a4ae3
predystopic-dev <azure@localhost.localdomain>
2025-07-20 12:04:24 +0530

    Modified the config

--- src/config.ts
+++ src/config.ts
@@ -3,34 +3,34 @@ import type { AccentColor, BaseColor } from "./colors";
 // IMPORTANT SET THOSE TWO VARIABLES:
 // NUMBER ONE:
 // if deployed to github pages, set to https://<your-github-username>.github.io/
-export const SITE = "https://flo-bit.dev";
+export const SITE = "https://predystopic-dev.github.io";
 
 // NUMBER TWO:
 // if repo name is <your-github-username>.github.io set to '/'
 // otherwise set to '/<your-repo-name>'
-export const BASE = "/blog-template";
+export const BASE = "/scribblings";
 
 // MORE SETTINGS:
 
 // will be used for the the title and meta tags and in the header (if SITE_NAME is left blank)
-export const SITE_TITLE = "flo-bit's blog template";
+export const SITE_TITLE = "Scribblings";
 
 // will be used in the meta tags (and for example shown in search results)
-export const SITE_DESCRIPTION = "Welcome to my blog template! Fork it and make it your own.";
+export const SITE_DESCRIPTION = "Welcome, here I write.";
 
 // will be used as the icon in the header and the favicon
 export const SITE_FAVICON = "🙃";
 
 // will be used in the footer as the name of the author (c) <YEAR> <NAME> - LICENSE
-export const NAME = "flo-bit";
+export const NAME = "Dev";
 
 // will be used in the footer as the license of the content (e.g. "All right reserved" or "CC-BY-SA 4.0")
 export const LICENSE = "MIT licensed.";
 
 // will be used to identify your bluesky account, so that likes and comments can be shown on your posts
-export const BLUESKY_IDENTIFIER = "flo-bit.dev";
+export const BLUESKY_IDENTIFIER = "predys-caterpillar";
 
-export const SOURCE_LINK = 'https://github.com/flo-bit/blog-template';
+export const SOURCE_LINK = 'https://github.com/predystopic-dev/scribblings';
 
 // will be used to set the base color of the blog
 export const BASE_COLOR: BaseColor = "neutral";
@@ -52,9 +52,9 @@ export const SOCIAL_LINKS: {
   SHOW_RSS?: boolean;
 } = {
   SHOW_RSS: true,
-  BLUESKY_URL: "https://bsky.app/profile/flo-bit.dev",
-  GITHUB_URL: "https://github.com/flo-bit",
-  EMAIL: "flo.bit.dev@gmail.com",
+  BLUESKY_URL: "https://bsky.app/profile/predys-caterpillar",
+  GITHUB_URL: "https://github.com/predystopic-dev",
+  EMAIL: "",
 };
 
 
